8fc69d5339d5d04b371c5964b8b5196759f7c5b4
ayivi <selormkwasi@gmail.com>
2024-10-22 17:58:10 +0000

    Initial Commit

---
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Ayivi on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pomodoro TimerTests/Pomodoro_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pomodoro_TimerTests.swift
+//  Pomodoro TimerTests
+//
+//  Created by Ayivi on 22/10/2024.
+//
+
+import Testing
+@testable import Pomodoro_Timer
+
+struct Pomodoro_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
